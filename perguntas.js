67cbe6014e698f2473948abc41b29f0d6138eaac
Fuolipe <143820336+fuolipe@users.noreply.github.com>
2024-11-22 12:05:00 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Informações sobre Gatos 1',
+    'Gatos tem raças?',
+    'SIMM, Gtos tem raças, um exemplo é a raça Maine Coon'
+)
+
+criaCartao(
+    'Informações sobre Gatos 2',
+    'Qual a especie dos gatos domesticos?',
+    'Felis Catus'
+)
+
+criaCartao(
+    'Informações sobre Gatos 3',
+    'Como os gatos nos veem?',
+    'os nossos gatos, acabam por nos ver como semelhantes a eles, como um gato gigante'
+)
+
+criaCartao(
+    'Informações sobre Gatos 4',
+    'Qual a maior raçade gato?',
+    'a raça Maine Coon, que pode chegar de 48 cm, a 1m de comprimento'
+)
